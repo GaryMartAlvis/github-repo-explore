846e8ad533888852f14020c15f77f3cd067cf8be
GaryMartAlvis <gary.martinez.alvis@gmail.com>
2024-07-03 10:31:44 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  CLIENT_ID: 'tu_client_id',
-  CLIENT_SECRET: 'tu_client_secret',
+  CLIENT_ID: 'Ov23li3yEXnipdPdNLzf',
+  CLIENT_SECRET: 'd6b379b3d05b4c32de4013792d75d3c5a37b488b',
   REDIRECT_URL: 'https://' + chrome.runtime.id + '.chromiumapp.org/'
 };
