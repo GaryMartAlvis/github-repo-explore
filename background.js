4162a8ccbe65b0fc678b887aa27484511e127573
GaryMartAlvis <gary.martinez.alvis@gmail.com>
2024-07-03 10:51:40 -0400

    Corrección de error de autentificación #2

--- background.js
+++ background.js
@@ -12,47 +12,24 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
 function authenticate() {
   return new Promise((resolve, reject) => {
-    const authUrl = 'https://github.com/login/oauth/authorize?' +
-      'client_id=' + CONFIG.CLIENT_ID +
-      '&redirect_uri=' + encodeURIComponent(CONFIG.REDIRECT_URL) +
-      '&scope=repo';
-
-    chrome.identity.launchWebAuthFlow({
-      url: authUrl,
-      interactive: true
-    }, (redirectUrl) => {
+    chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
       if (chrome.runtime.lastError) {
         reject(chrome.runtime.lastError);
       } else {
-        const code = new URL(redirectUrl).searchParams.get('code');
-        getAccessToken(code).then(resolve).catch(reject);
+        resolve(token);
       }
     });
   });
 }
 
-function getAccessToken(code) {
-  return fetch('https://github.com/login/oauth/access_token', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json'
-    },
-    body: JSON.stringify({
-      client_id: CONFIG.CLIENT_ID,
-      client_secret: CONFIG.CLIENT_SECRET,
-      code: code
-    })
-  })
-  .then(response => response.json())
-  .then(data => {
-    if (data.error) {
-      throw new Error(data.error_description || 'Error al obtener el token de acceso');
-    }
-    return data.access_token;
-  });
-}
-
 function refreshToken() {
-  return authenticate();
+  return new Promise((resolve, reject) => {
+    chrome.identity.getAuthToken({ 'interactive': false }, function(token) {
+      if (chrome.runtime.lastError) {
+        reject(chrome.runtime.lastError);
+      } else {
+        resolve(token);
+      }
+    });
+  });
 }
