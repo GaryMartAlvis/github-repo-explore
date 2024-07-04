1749d00cb7f54bc8b822e3ac377df92a24c5ba66
GaryMartAlvis <gary.martinez.alvis@gmail.com>
2024-07-04 15:21:19 -0400

    Update: Corrección de autentificación.

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 export const CONFIG = {
   CLIENT_ID: 'Ov23li3yEXnipdPdNLzf',
   CLIENT_SECRET: '42b829ac05547eaf9f777419f0e3dab347e804db',
-  REDIRECT_URL: 'https://' + chrome.runtime.id + '.chromiumapp.org/'
+  REDIRECT_URL: 'https://' + chrome.runtime.id + '.chromiumapp.org/',
+  SCOPES: "repo"
 };
